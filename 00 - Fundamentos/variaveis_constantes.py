0290c04d0f06dbad992eff2e742a473195bbc321
Wellington Santos Souza <74972381+welli45@users.noreply.github.com>
2023-10-28 15:32:49 -0300

    Update variaveis_constantes.py
    x

--- 00 - Fundamentos/variaveis_constantes.py
+++ 00 - Fundamentos/variaveis_constantes.py
@@ -1,12 +1,12 @@
 nome = "Guilherme"
 idade = 28
-
+# podemos declarar as variáveis na mesma linha desde que separamos por vírgula. 
 nome, idade = "Giovanna", 27
 
 print(nome, idade)
-
+# snake case
 limite_saque_diario = 1000
-
+# constante
 BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
 
 print(BRAZILIAN_STATES)
