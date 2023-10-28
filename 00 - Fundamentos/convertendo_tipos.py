72ae7c3ead29d7466e2009cdce26ce3412d4b61d
Wellington Santos Souza <74972381+welli45@users.noreply.github.com>
2023-10-28 15:38:57 -0300

    Update convertendo_tipos.py
    s

--- 00 - Fundamentos/convertendo_tipos.py
+++ 00 - Fundamentos/convertendo_tipos.py
@@ -1,9 +1,16 @@
+## para fazer a conversão de um tipo para outro é só usar os construtoroes ##
+## lembre-se, existem tipos que não podemos converter ## 
+# convertento float para int 
 print(int(1.97348728))
+# convertendo string pra int
 print(int("10"))
+# convertendo string para float
 print(float("10.10"))
+# convertendo int para float 
 print(float(100))
 
 valor = 10
+
 valor_str = str(valor)
 print(type(valor))
 print(type(valor_str))
